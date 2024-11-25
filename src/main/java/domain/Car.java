3501dff42657d3409b57fa02675443ae73f7d31c
sia <aaay8228@gmail.com>
2024-11-25 15:44:53 +0900

    feat: 자동차 기능 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,38 @@
+package domain;
+
+public class Car {
+	private static final Integer MAX_LENGTH = 5;
+	private static final Integer MIN_ACCELERATION = 4;
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		validName(name);
+		this.name = name;
+		this.position = 0; // 0으로 초기화
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	private void validName(String name) {
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+		}
+		if (name.length() > MAX_LENGTH) {
+			throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+		}
+	}
+
+	public int getPosition() {
+		return this.position;
+	}
+
+	public void move(int acceleration) {
+		// 4이상의 가속도가 있는 경우만 앞으로 갈 수 있다.
+		if (acceleration >= MIN_ACCELERATION) {
+			this.position++;
+		}
+	}
+}
---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,53 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CarTest {
+
+	@Test
+	void 각각의_자동차는_이름을_가질_수_있다() {
+		String name = "pobi";
+		Car car = new Car(name);
+		String expected = "pobi";
+
+		assertThat(car.getName()).isEqualTo(expected);
+	}
+
+	@Test
+	void 자동차_이름은_5글자를_넘어서는_안됩니다() {
+		String name = "sonata";
+
+		assertThatThrownBy(() -> new Car(name))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다");
+	}
+
+	@Test
+	void 자동차의_처음_위치는_0입니다() {
+		String name = "pride";
+		Car car = new Car(name);
+
+		assertThat(car.getPosition()).isEqualTo(0);
+	}
+
+	@Test
+	void 자동차는_4이상의_가속도가_주어질때_앞으로_움직입니다() {
+		String name = "pride";
+		Car car = new Car(name);
+		car.move(4);
+
+		assertThat(car.getPosition()).isEqualTo(1);
+	}
+
+	@Test
+	void 자동차는_4이하의_가속도가_주어질때_제자리를_유지합니다() {
+		String name = "pride";
+		Car car = new Car(name);
+		car.move(3);
+
+		assertThat(car.getPosition()).isEqualTo(0);
+	}
+}
